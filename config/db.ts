68c75cb7d2dc46a8139033e2aaa567d8b1dbf134
Murilo Luciano <murilo.luciano123@gmail.com>
2024-05-26 16:46:56 -0400

    fix: Fixing mongo connection

--- config/db.ts
+++ config/db.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export default async function connectDB() {
   console.info("[mongo] Connecting...");
 
-  if (!process.env.DATABASE_URL) throw new Error("No DATABASE_URL defined");
+  if (!process.env.MONGODB_URI) throw new Error("No MONGODB_URI defined");
 
-  await mongoose.connect(process.env.DATABASE_URL);
+  await mongoose.connect(process.env.MONGODB_URI);
 
   console.info("Mongo connected!");
 }
