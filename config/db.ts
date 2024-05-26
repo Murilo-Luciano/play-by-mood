62d666f9b5fa31eabde94fbb9c2786a955e63214
Murilo Luciano <murilo.luciano123@gmail.com>
2024-05-26 16:37:48 -0400

    feat: connectDB log

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export default async function connectDB() {
+  console.info("[mongo] Connecting...");
+
   if (!process.env.DATABASE_URL) throw new Error("No DATABASE_URL defined");
 
   await mongoose.connect(process.env.DATABASE_URL);
