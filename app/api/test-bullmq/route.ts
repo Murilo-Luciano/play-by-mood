15b57a68a2e10516013b20224aa1a0ee53983d06
Murilo Luciano <murilo.luciano123@gmail.com>
2024-07-17 09:27:01 -0400

    Test worker

--- app/api/test-bullmq/route.ts
+++ app/api/test-bullmq/route.ts
@@ -1,56 +1,5 @@
-import connectDB from "@/config/db";
-import TestModel from "@/models/TesteBull";
-import { FlowProducer, Queue, QueueEvents, Worker } from "bullmq";
+import { queue } from "@/workers/test.worker";
 import { NextRequest, NextResponse } from "next/server";
-
-const connection = {
-  host: process.env.REDIS_HOST_URL,
-  port: 12972,
-  password: process.env.REDIS_PASSWORD,
-};
-
-const queue = new Queue("Teste", {
-  connection,
-});
-
-const worker = new Worker(
-  "Teste",
-  async (job) => {
-    console.log(`HANDLING job ${job.data.testando}`);
-
-    await connectDB();
-
-    await TestModel.create({ testando: "SIM" });
-    console.log("AQUI!!! ----> ", await TestModel.countDocuments());
-
-    console.log(`FINISH HANDLING job ${job.data.testando}`);
-
-    return "HELLO WORLD";
-  },
-  {
-    connection,
-  }
-);
-
-const queueEvents = new QueueEvents("Teste", { connection });
-
-queueEvents.on("waiting", ({ jobId }) => {
-  console.log(`A job with ID ${jobId} is waiting`);
-});
-
-queueEvents.on("active", ({ jobId, prev }) => {
-  console.log(`Job ${jobId} is now active; previous status was ${prev}`);
-});
-
-queueEvents.on("completed", ({ jobId, returnvalue }) => {
-  console.log(`${jobId} has completed and returned ${returnvalue}`);
-});
-
-queueEvents.on("failed", ({ jobId, failedReason }) => {
-  console.log(`${jobId} has failed with reason ${failedReason}`);
-});
-
-const flowProducer = new FlowProducer({ connection });
 
 export async function GET(request: NextRequest) {
   console.log("[bullqueueMQ] Handling test bullqueueMQ");
