488d0e2a651fe8d8b04167b27b8b2a3a03656906
Murilo Luciano <murilo.luciano123@gmail.com>
2024-07-17 16:40:03 -0400

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { Mood } from "@/services/gameService";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl">WIP...</h1>
+      <div className="bg-slate-500 flex gap-4">
+        {Object.values(Mood).map((mood, i) => {
+          return (
+            <button className="bg-red-700" key={i}>
+              {mood}
+            </button>
+          );
+        })}
+      </div>
     </main>
   );
 }
